01d457b350ce5c4706de41528ca1bcf18412a7c5
JavierGior <javiergiordano@gmail.com>
2021-11-23 11:03:05 -0300

    Update main.js

--- code/main.js
+++ code/main.js
@@ -1,5 +1,5 @@
 var scene = new THREE.Scene();
-scene.fog = new THREE.FogExp2("#1a1a1a", 0.01);
+scene.fog = new THREE.FogExp2("white", 0.001);
 var gui = new dat.GUI();
 var clock = new THREE.Clock();
 
