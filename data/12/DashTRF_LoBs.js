4d28305ce4dff22b27b177813db9ed2b53d3c4f8
JavierGior <javiergiordano@gmail.com>
2022-06-12 12:37:18 -0300

    Add files via upload

---
+++ data/12/DashTRF_LoBs.js
@@ -0,0 +1,81 @@
+const DashTRF_LoBs = [
+  {
+    YearDT: "2022",
+
+    MonthDT: "1",
+
+    LoB: "Finance",
+
+    ProyectosEnBacklog: "0",
+  },
+
+  {
+    YearDT: "2022",
+
+    MonthDT: "1",
+
+    LoB: "Insurance",
+
+    ProyectosEnBacklog: "0",
+  },
+
+  {
+    YearDT: "2022",
+
+    MonthDT: "1",
+
+    LoB: "OPS",
+
+    ProyectosEnBacklog: "0",
+  },
+
+  {
+    YearDT: "2022",
+
+    MonthDT: "1",
+
+    LoB: "Risk",
+
+    ProyectosEnBacklog: "0",
+  },
+
+  {
+    YearDT: "2022",
+
+    MonthDT: "1",
+
+    LoB: "Wholesale",
+
+    ProyectosEnBacklog: "0",
+  },
+
+  {
+    YearDT: "2022",
+
+    MonthDT: "1",
+
+    LoB: "WPB",
+
+    ProyectosEnBacklog: "0",
+  },
+
+  {
+    YearDT: "2022",
+
+    MonthDT: "1",
+
+    LoB: "WPB - RBB",
+
+    ProyectosEnBacklog: "0",
+  },
+
+  {
+    YearDT: "2022",
+
+    MonthDT: "1",
+
+    LoB: "xLoB",
+
+    ProyectosEnBacklog: "0",
+  },
+];
